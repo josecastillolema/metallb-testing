e21e8183c33cf90233894b4d7cade17d0a3d4ee7
Murali Krishnasamy <70236227+mukrishn@users.noreply.github.com>
2022-02-15 16:29:43 -0500

    Create run_maxservices_latency_test_fromgit_metallb.sh

---
+++ e2e-kubeburner/run_maxservices_latency_test_fromgit_metallb.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+WORKLOAD_TEMPLATE=workloads/max-services/max-services_latency_metallb.yml
+METRICS_PROFILE=${METRICS_PROFILE:-metrics-profiles/metrics-aggregated.yaml}
+export TEST_JOB_ITERATIONS=${SERVICE_COUNT:-1}
+export WORKLOAD=max-services
+
+. common.sh
+
+deploy_operator
+check_running_benchmarks
+if [[ ${PPROF_COLLECTION} == "true" ]] ; then
+  delete_pprof_secrets
+  delete_oldpprof_folder
+  get_pprof_secrets
+fi 
+./tcping_check.sh &
+run_workload kube-burner-crd.yaml
+rc=$?
+if [[ ${CLEANUP_WHEN_FINISH} == "true" ]]; then
+  cleanup ${WORKLOAD}
+fi
+delete_pprof_secrets
+
+if [[ ${ENABLE_SNAPPY_BACKUP} == "true" ]] ; then
+  snappy_backup kube-burner-maxservices
+fi
+
+exit ${rc}
